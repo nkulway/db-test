aa05fa982b28918fb5d4abb4bf15ac290e4445a9
Nick Kulway <nkulway@gmail.com>
2021-11-10 23:30:09 -0500

    add any pending changes

---
+++ server.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const routes = require('./routes');
+const bodyParser = require('body-parser')
+const logger = require('morgan');
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json())
+
+app.use('/api', routes);
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+
+
+app.use(logger('dev'))
+
